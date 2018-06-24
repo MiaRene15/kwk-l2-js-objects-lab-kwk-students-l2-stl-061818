ced656bb6b517f72980484e8fdd8a8b4dd7f7e3e
MiaRene15 <kids.hines@icloud.com>
2018-06-24 20:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const driver = {
 }
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-   let x = driver.name = 'Sam'; driver.address = ''
+   let x = driver.name = 'Sam'; driver.address = '11 Broadway'
    return x 
    
 }
